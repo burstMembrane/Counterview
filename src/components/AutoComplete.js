76b3c3b59c2f5c99a3a95d46f77082ce1dd238cf
Liam Power <liamfpower@gmail.com>
2021-10-15 18:17:26 +1100

    added autocomplete

---
+++ src/components/AutoComplete.js
@@ -0,0 +1,134 @@
+import { useState, useEffect } from "react";
+import FakeData from "../data/fakedata.json";
+import OGPepe from "../data/og_pepe.json";
+
+import { Dropdown, FormControl, InputGroup, Badge } from "react-bootstrap";
+
+export default function AutoComplete(props) {
+  const [autoComplete, setAutoComplete] = useState([]);
+  const [showComplete, setShowComplete] = useState(false);
+  const [addresses, setAddresses] = useState([]);
+
+  const { input, setInput, search } = props;
+
+  //   get asset names
+  const assets = [
+    ...FakeData.map((obj) => {
+      return { series: obj.series, asset: obj.asset };
+    }),
+    ...OGPepe.map((obj) => {
+      return { series: obj.series, asset: obj.asset };
+    }),
+  ];
+
+  const handleSearch = async (value) => {
+    // If text input is not length of address then search for asset
+
+    if (value.length < 33 && !parseInt(input[0])) {
+      // setResult(Fetch("asset", searchValue))
+
+      setShowComplete(false);
+      setAutoComplete([]);
+      search(input);
+    }
+    if (parseInt(input[0]) === 1 && input.length >= 25 && input.length <= 35) {
+      setAddresses([...new Set([value, ...addresses])]);
+      const JSONAddresses = JSON.stringify(addresses);
+
+      localStorage.setItem("addresses", JSONAddresses);
+
+      // Search an address
+      // setResult(Fetch("address", searchValue))
+    }
+  };
+
+  function handleInput(input) {
+    setShowComplete(false);
+    setAutoComplete([]);
+    // check if it's an asset
+    if (input.length > 0 && !parseInt(input[0])) {
+      setAutoComplete(
+        assets.filter((asset) => asset.asset.includes(input.toUpperCase()))
+      );
+      if (autoComplete.length > 0) setShowComplete(true);
+    }
+    // if it starts with 1, it's probably an address
+    if (parseInt(input[0]) === 1 && addresses.length >= 1) {
+      setShowComplete(true);
+    }
+    setInput(input);
+  }
+  useEffect(() => {
+    try {
+      const lsAdresses = JSON.parse(localStorage.getItem("addresses"));
+
+      if (lsAdresses !== null) setAddresses(lsAdresses);
+    } catch (error) {
+      console.log("couldn't parse addresses");
+    }
+  }, []);
+
+  return (
+    <InputGroup>
+      {showComplete && (
+        <Dropdown.Menu show as='div' className='w-100'>
+          {autoComplete.map(
+            (asset, i) =>
+              i < 10 && (
+                <Dropdown.Item
+                  className='w-100'
+                  onClick={(e) => {
+                    handleSearch(e.target.innerText);
+
+                    setShowComplete(false);
+                  }}
+                  key={i}>
+                  <span align='start'>{asset.asset}</span>
+                  <Badge
+                    bg='light'
+                    variant='light'
+                    style={{ float: "right", color: "#212121" }}
+                    pill
+                    className='ml-auto'>
+                    {asset.series && asset.series}
+                  </Badge>
+                </Dropdown.Item>
+              )
+          )}
+          {addresses.length >= 1 && (
+            <Dropdown.Header>Address History</Dropdown.Header>
+          )}
+          {addresses.length >= 1 &&
+            addresses.map(
+              (address, i) =>
+                i < 5 && (
+                  <Dropdown.Item
+                    className='text-truncate'
+                    style={{ textOverflow: "ellipsis" }}
+                    as='div'
+                    onClick={(e) => {
+                      handleSearch(e.target.innerText);
+
+                      setShowComplete(false);
+                    }}
+                    variant='secondary'
+                    key={i}>
+                    {address}
+                  </Dropdown.Item>
+                )
+            )}
+        </Dropdown.Menu>
+      )}
+      <FormControl
+        type='search'
+        value={input}
+        placeholder={"Search asset or address"}
+        className='mr-2 search-field'
+        aria-label='Search'
+        onChange={(e) => {
+          handleInput(e.target.value);
+        }}
+      />
+    </InputGroup>
+  );
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,95 +1,98 @@
-import React, { useState, useEffect } from "react"
-import { Container, Nav, Navbar, Form, FormControl, Button } from "react-bootstrap"
-import Fetch from "./Fetch"
-
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import Fetch from "./Fetch";
+import AutoComplete from "./AutoComplete";
 function NavBar(props) {
-    const [ input, setInput ] = useState("")    // Search input box value
-    const [ result, setResult ] = useState("")  // Fetch results
+  const [input, setInput] = useState(""); // Search input box value
+  const [result, setResult] = useState(""); // Fetch results
 
-    // 1. Handles search input
-    function handleInput(input) {
-        setInput(input)
+  // 1. Handles search input
+  function handleInput(input) {
+    setInput(input);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  // 2. Receives search input and filters length to determine if asset or address then fetches API data
+  const search = async (value) => {
+    // If text input is not length of address then search for asset
+    if (value.length < 33) {
+      const res = await Fetch("asset", value);
+      setResult(res);
+    } else {
+      // Search an address
+      const res = await Fetch("balances", value);
+      setResult(res);
     }
+  };
 
-    function handleSubmit(e) {
-        e.preventDefault();
-    }
+  // 3. Updates wallet values when async is complete
+  useEffect(() => {
+    // Passes JSON data to parent
+    props.onDataChange(result);
+  }, [result]);
 
-    // 2. Receives search input and filters length to determine if asset or address then fetches API data
-    const search = async (value) => {
-
-        // If text input is not length of address then search for asset
-        if(value.length < 33) {
-            const res = await Fetch("asset", value)
-            setResult(res)
-        } else {
-        // Search an address
-            const res = await Fetch("balances", value)
-            setResult(res)
-        }
-    }
-
-    // 3. Updates wallet values when async is complete
-    useEffect(() => {
-        // Passes JSON data to parent
-        props.onDataChange(result)
-    }, [result])
-
-    return (
-        <Navbar collapseOnSelect className="navBar" expand="lg"variant="dark">
-            <Container>
-                <Navbar.Brand href="" className="logo">Counterview.club</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link 
-                        className={props.pepeFilter.original ? "selected-filter" : "nav-link"}
-                        onClick={() => {
-                            props.onFilterChange("original")
-                        }}
-                    >OG Peps</Nav.Link>
-                    <Nav.Link 
-                        className={props.pepeFilter.fake ? "selected-filter" : "nav-link"}
-                        onClick={() => {
-                            props.onFilterChange("fake")
-                        }}
-                    >Fakes</Nav.Link>
-                    <Nav.Link 
-                        href="https://fapep.github.io/FABRIQUE/"
-                        target="_blank"
-                    >About</Nav.Link>
-                </Nav>
-                <Form 
-                    id="search-input"
-                    className="d-flex"
-                    onClick={(e) => {
-                        // console.log(e.target)
-                    }}
-                    onSubmit={(e) => {
-                        handleSubmit(e)
-                    }}
-                >
-                <FormControl
-                    type="search"
-                    placeholder="Search asset or address"
-                    className="mr-2 search-field"
-                    aria-label="Search"
-                    onChange={(e)=>{
-                        handleInput(e.target.value)
-                    }}
-                />
-                <Button 
-                    variant="outline-success"
-                    className="search-button"
-                    onClick={() => {
-                        search(input)
-                    }}
-                >Search</Button>
-                </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar collapseOnSelect className='navBar' expand='lg' variant='dark'>
+      <Container>
+        <Navbar.Brand href='' className='logo'>
+          Counterview.club
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link
+              className={
+                props.pepeFilter.original ? "selected-filter" : "nav-link"
+              }
+              onClick={() => {
+                props.onFilterChange("original");
+              }}>
+              OG Peps
+            </Nav.Link>
+            <Nav.Link
+              className={props.pepeFilter.fake ? "selected-filter" : "nav-link"}
+              onClick={() => {
+                props.onFilterChange("fake");
+              }}>
+              Fakes
+            </Nav.Link>
+            <Nav.Link href='https://fapep.github.io/FABRIQUE/' target='_blank'>
+              About
+            </Nav.Link>
+          </Nav>
+          <AutoComplete setInput={setInput} search={search} input={input} />
+          <Form
+            id='search-input'
+            className='d-flex'
+            onClick={(e) => {
+              // console.log(e.target)
+            }}
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}>
+            <Button
+              variant='outline-success'
+              className='search-button'
+              onClick={() => {
+                search(input);
+              }}>
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
